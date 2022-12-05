25c0001cb71713d88a575520f9bf079529d2afe1
Mariya Andreeva <mashanasha_1@example.cdotnet>
2022-12-05 10:47:33 +0600

    Add a solution of T2

---
+++ Task2/Program.cs
@@ -0,0 +1,44 @@
+﻿/*Задайте одномерный массив, заполненный случайными числами. 
+Найдите сумму элементов, стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0*/
+
+int size = ReadInt("Введите размер массива: ");
+int[] numbers = new int[size];
+int sum = 0;
+
+FillArrayRandomNumbers(numbers, 100, 999);
+WriteArray(numbers);
+
+for(int i = 0; i < numbers.Length; i++)
+{
+if (i % 2 != 0)
+{
+    sum = sum + numbers [i];
+}
+}
+Console.WriteLine(sum);
+
+
+void FillArrayRandomNumbers(int[] array, int min, int max)
+{
+for(int i = 0; i < array.Length; i++)
+{
+array[i] = new Random().Next(min, max + 1);
+}
+}
+
+void WriteArray(int[] array)
+{
+for(int i = 0; i < array.Length; i++)
+{
+Console.Write(array[i] + " ");
+}
+Console.WriteLine();
+}
+
+int ReadInt(string message)
+{
+Console.Write(message);
+return Convert.ToInt32(Console.ReadLine());
+}
